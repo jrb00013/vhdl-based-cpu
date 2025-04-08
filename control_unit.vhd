8e421f6400ba6481ecff6166714879b95ec228de
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-04-08 09:56:19 -0400

    opened more ports

--- control_unit.vhd
+++ control_unit.vhd
@@ -1,12 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity control_unit is
-    Port ( opcode : in STD_LOGIC_VECTOR(5 downto 0);
-           control_signals : out STD_LOGIC_VECTOR(15 downto 0);
-           -- other control signals
+    Port ( opcode : in STD_LOGIC_VECTOR(3 downto 0);
+           reg_write : out STD_LOGIC;
+           alu_op : out STD_LOGIC_VECTOR(3 downto 0);
+           mem_read : out STD_LOGIC;
+           mem_write : out STD_LOGIC;
+           branch : out STD_LOGIC;
+           mem_to_reg : out STD_LOGIC;
+           alu_src : out STD_LOGIC
            );
 end control_unit;
 
@@ -14,10 +17,32 @@ architecture Behavioral of control_unit is
 begin
     process(opcode)
     begin
-        -- Implement control logic based on opcode
         case opcode is
-            when "000000" => control_signals <= "0000000000000001"; -- Example
-            when others => control_signals <= "0000000000000000"; -- Default
+            when "0000" => -- ADD instruction
+                reg_write <= '1';
+                alu_op <= "0000";  -- ALU add
+                mem_read <= '0';
+                mem_write <= '0';
+                branch <= '0';
+                mem_to_reg <= '0';
+                alu_src <= '0';
+            when "0001" => -- SUB instruction
+                reg_write <= '1';
+                alu_op <= "0001";  -- ALU subtract
+                mem_read <= '0';
+                mem_write <= '0';
+                branch <= '0';
+                mem_to_reg <= '0';
+                alu_src <= '0';
+            -- Add more instructions as necessary
+            when others =>
+                reg_write <= '0';
+                alu_op <= "0000";  -- Default ALU operation
+                mem_read <= '0';
+                mem_write <= '0';
+                branch <= '0';
+                mem_to_reg <= '0';
+                alu_src <= '0';
         end case;
     end process;
 end Behavioral;
